8a3216c84240cb048fbdc22b7442deece071b283
jesus07098 <jesus07098@gmail.com>
2020-05-07 07:08:25 -0400

    seleccion de elementos

--- practicas/script.js
+++ practicas/script.js
@@ -3,7 +3,7 @@ const titleH1 = document.querySelector('h1#title');
 const divContainer = document.querySelector('div#container');
 
 btnChangeState.addEventListener('dblclick', function(){
- divContainer.classList.add('d-none');
- titleH1.classList.add('font-italic');
- titleH1.classList.add('text-danger');
+    divContainer.classList.add('d-none');
+    titleH1.classList.add('font-italic');
+    titleH1.classList.add('text-danger');
 });
