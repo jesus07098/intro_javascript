a53201c445232347ac2627743c5e2e8135482aba
jesus07098 <jesus07098@gmail.com>
2020-05-07 07:12:29 -0400

    seleccion de elementos 1

--- practicas/script.js
+++ practicas/script.js
@@ -1,9 +1,11 @@
+//seleccionando el boton, el h1 y el div
 const btnChangeState = document.querySelector("button#changeState");
 const titleH1 = document.querySelector('h1#title');
 const divContainer = document.querySelector('div#container');
 
 btnChangeState.addEventListener('dblclick', function(){
-    divContainer.classList.add('d-none');
-    titleH1.classList.add('font-italic');
-    titleH1.classList.add('text-danger');
+    
+    divContainer.classList.add('d-none'); //agregando la clase al div para ocultarlo
+    titleH1.classList.add('font-italic'); //agregando la clase al h1 para ponerlo en cursiva
+    titleH1.classList.add('text-danger'); //agregando la clase al h1 para ponerle el color rojo
 });
